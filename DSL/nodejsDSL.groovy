528eee7e8ff3a9223003f5e99493d7a6183e9bad
boni-git-devops <abonbal829@gmail.com>
2025-04-23 11:43:58 +0200

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/boni-git-devops/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('boni-git-devops')
+            node / gitConfigEmail('abonbal829@gmail.com')
         }
     }
     triggers {
